ca62f82b6aac09edd184b18e0c6c922d9c3306da
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-18 17:33:23 +0530

    Update temperature.sol

--- contracts/temperature.sol
+++ contracts/temperature.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.5.0 <0.9.0;
 contract temperature {
     function checkTemp(uint256 temp) public pure returns (string memory) {
         if (temp < 30) {
-            return "Do delivery to B and pay to A";
+            return /*Type you stament here in double quotes*/;
         } else {
-            return "Do not do delivery to B and do not pay to A";
+            return /*Type you stament here in double quotes*/;
         }
     }
 }
